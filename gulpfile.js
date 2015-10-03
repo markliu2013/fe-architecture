2404c10b0b861eee1a739e5466047fc0b2cdd0bd
markliu2013 <markliu2013@gmail.com>
2015-10-03 18:50:31 +0800

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,56 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     autoprefix = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
     cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
+    sourcemaps = require('gulp-sourcemaps'),
+    htmlreplace = require('gulp-html-replace'),
+    htmlmin = require('gulp-htmlmin'),
     del = require('del');
 
 gulp.task('css', function() {
     gulp.src('less/master.less')
         .pipe(less())
         .pipe(autoprefix({ browsers: ['> 1%', 'last 2 versions'] }))
-        .pipe(gulp.dest('dist/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(gulp.dest('dist/css'));
 });
 gulp.task('js', function() {
-    gulp.src('js/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
+    gulp.src(['js/jQuery.js', 'js/util.js', 'js/Grid.js', 'js/Bullet.js', 'js/Tank.js', 'js/NPCTank.js', 'js/MyTank.js', 'js/TankContainer.js', 'js/Game.js'])
+        .pipe(sourcemaps.init())
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
-        .pipe(notify({ message: 'Scripts task complete' }));
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('dist/js'));
 });
 gulp.task('images', function() {
     return gulp.src('images/**/*')
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('dist/images'))
-        .pipe(notify({ message: 'Images task complete' }));
+        .pipe(gulp.dest('dist/images'));
+});
+gulp.task('html', function() {
+    gulp.src('index.html')
+        .pipe(htmlreplace({
+            'css': 'css/master.min.css',
+            'js': 'js/main.min.js'
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(htmlmin({collapseWhitespace: true, conservativeCollapse:false, preserveLineBreaks:false, removeComments: true, removeCommentsFromCDATA: true}))
+        .pipe(gulp.dest('dist'));
+    ;
 });
 gulp.task('clean', function(cb) {
-    del(['dist/css', 'dist/js', 'dist/images'], cb)
+    del(['dist/*'], cb);
 });
 gulp.task('watch', function() {
     gulp.watch('less/*.less', ['css']);
     gulp.watch('js/*.js', ['js']);
+    gulp.watch('*.html', ['html']);
     gulp.watch('images/**/*', ['images']);
 });
+gulp.task('default', ['clean', 'css', 'js', 'html']);
