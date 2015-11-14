ab858d6b953ed4ac0fee7a19c4c5b866a1837a7b
markliu2013 <markliu2013@gmail.com>
2015-11-14 15:29:43 +0800

    dskies

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var gulp = require('gulp'),
     autoprefix = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     cache = require('gulp-cache'),
@@ -13,7 +12,7 @@ var gulp = require('gulp'),
     del = require('del');
 
 gulp.task('css', function() {
-    gulp.src('less/master.less')
+    gulp.src('less/*.less')
         .pipe(less())
         .pipe(autoprefix({ browsers: ['> 1%', 'last 2 versions'] }))
         .pipe(rename({suffix: '.min'}))
@@ -21,32 +20,36 @@ gulp.task('css', function() {
         .pipe(gulp.dest('dist/css'));
 });
 gulp.task('js', function() {
-    gulp.src(['js/jQuery.js', 'js/util.js', 'js/Grid.js', 'js/Bullet.js', 'js/Tank.js', 'js/NPCTank.js', 'js/MyTank.js', 'js/TankContainer.js', 'js/Game.js'])
-        .pipe(sourcemaps.init())
+    gulp.src(['bower_components/jquery/dist/jquery.min.js', 'js/jquery.fancyCountdown.min.js', 'js/swipe.js'])
+        //.pipe(sourcemaps.init())
         .pipe(concat('main.js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('../maps'))
+        //.pipe(rename({suffix: '.min'}))
+        //.pipe(uglify())
+        //.pipe(sourcemaps.write('../maps'))
         .pipe(gulp.dest('dist/js'));
 });
-gulp.task('images', function() {
-    return gulp.src('images/**/*')
-        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('dist/images'));
-});
 gulp.task('html', function() {
-    gulp.src('index.html')
+    gulp.src('index-pc.html')
         .pipe(htmlreplace({
-            'css': 'css/master.min.css',
-            'js': 'js/main.min.js'
+            'css': 'css/main.min.css',
+            'js': 'js/main.js'
         }))
         .pipe(gulp.dest('dist'))
-        .pipe(htmlmin({collapseWhitespace: true, conservativeCollapse:false, preserveLineBreaks:false, removeComments: true, removeCommentsFromCDATA: true}))
+        //.pipe(htmlmin({collapseWhitespace: true, conservativeCollapse:false, preserveLineBreaks:false, removeComments: true, removeCommentsFromCDATA: true}))
         .pipe(gulp.dest('dist'));
     ;
 });
+// Copy all other files to dist directly
+gulp.task('copy', function() {
+    //copy images
+    gulp.src('img/*')
+        .pipe(gulp.dest('dist/img'));
+    //copy fonts
+    gulp.src(['less/*.eot','less/*.svg','less/*.ttf','less/*.woff'])
+        .pipe(gulp.dest('dist/css'));
+});
 gulp.task('clean', function(cb) {
-    del(['dist/*'], cb);
+    del(['dist'], cb);
 });
 gulp.task('watch', function() {
     gulp.watch('less/*.less', ['css']);
@@ -54,4 +57,4 @@ gulp.task('watch', function() {
     gulp.watch('*.html', ['html']);
     gulp.watch('images/**/*', ['images']);
 });
-gulp.task('default', ['clean', 'css', 'js', 'html']);
+gulp.task('default', ['clean', 'css', 'js', 'html', 'copy']);
